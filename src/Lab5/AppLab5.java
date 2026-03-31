894e71fbc54a1d7a1f7b32fb4c0f757e64d733b9
NeneaTantiUwU <annhasdeu@gmail.com>
2026-03-31 09:17:33 +0300

    init

---
+++ src/Lab5/AppLab5.java
@@ -0,0 +1,37 @@
+package Lab5;
+
+public class AppLab5 {
+
+    public class IntCalculator {
+
+        private int state;
+
+        public IntCalculator(int initialValue) {
+            this.state = initialValue;
+        }
+
+        public IntCalculator add(int value) {
+            this.state += value;
+            return this;
+        }
+
+        public IntCalculator subtract(int value) {
+            this.state -= value;
+            return this;
+        }
+
+        public IntCalculator multiply(int value) {
+            this.state *= value;
+            return this;
+        }
+
+        public int result() {
+            return this.state;
+        }
+
+        public void clear() {
+            this.state = 0;
+        }
+    }
+}
+
